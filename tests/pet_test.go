fcfd98b777afdc23d067b9caa8fc4f87bb3f1efe
Don Johnson <dj@codetestcode.io>
2022-09-09 19:56:08 -0700

    added pet upload test

--- tests/pet_test.go
+++ tests/pet_test.go
@@ -3,7 +3,13 @@ package tests
 import (
 	"bytes"
 	"encoding/json"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"mime/multipart"
 	"net/http"
+	"os"
+	"path/filepath"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -88,4 +94,43 @@ func TestAddNewPetToStoreReturnsPayload(t *testing.T) {
 	req.Header.Add("Content-Type", "application/json")
 	res, _ := client.Do(req)
 	assert.NotEmpty(t, res.Body)
+}
+
+func TestUploadPetImageReturnStatusCodeOk(t *testing.T) {
+	url := "https://petstore.swagger.io/v2/pet/666/uploadImage"
+	method := "POST"
+	payload := &bytes.Buffer{}
+	writer := multipart.NewWriter(payload)
+	file, errFile1 := os.Open("data/sample.txt")
+	defer file.Close()
+	part1,
+		errFile1 := writer.CreateFormFile("file", filepath.Base("/home/ops/Documents/HIPPA.pdf"))
+	_, errFile1 = io.Copy(part1, file)
+	if errFile1 != nil {
+		return
+	}
+	err := writer.Close()
+	if err != nil {
+		return
+	}
+	client := &http.Client{}
+	req, err := http.NewRequest(method, url, payload)
+
+	if err != nil {
+		return
+	}
+	req.Header.Set("Content-Type", writer.FormDataContentType())
+	res, err := client.Do(req)
+	if err != nil {
+		return
+	}
+	defer res.Body.Close()
+
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	assert.Equal(t, res.StatusCode, 200)
+	assert.NotEmpty(t, string(body))
 }
